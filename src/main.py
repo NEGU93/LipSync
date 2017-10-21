8bd2270fc4214c0fa1b167811f6f0d09a69459a4
NEGU93 <joseagustin.barra@gmail.com>
2017-10-21 16:27:58 -0300

    menu added

--- src/main.py
+++ src/main.py
@@ -1,27 +1,67 @@
-from PyQt5 import QtWidgets
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, databaseFilePath, userFilePath):
-        super(MainWindow, self).__init__()
-        self.databaseFilePath = databaseFilePath
-        self.userFilePath = userFilePath
-        self.createUI()
 
-    def changeFilePath(self):
-        print('changeFilePath')
+class LipSyncData():
+    def __init__(self):
+        self.audio = []
 
-    def createUI(self):
-        self.setWindowTitle('Equipment Manager 0.3')
-        menu = self.menuBar().addMenu('File')
-        action = menu.addAction('Open')
-        action.triggered.connect(self.changeFilePath)
+    def open_wav(self, path):
+        print('Open wav file with this path:' + path)
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = 'LipSync - by Matias Dwek & Agustin Barrachina'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 400
+        self.data = LipSyncData()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.addMenu()
+
+        self.show()
+
+    def addMenu(self):
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('File')
+        editMenu = mainMenu.addMenu('Edit')
+        viewMenu = mainMenu.addMenu('View')
+        searchMenu = mainMenu.addMenu('Search')
+        toolsMenu = mainMenu.addMenu('Tools')
+        helpMenu = mainMenu.addMenu('Help')
+
+        # Under File
+        # Open File
+        openButton = QAction('Open', self)
+        openButton.setShortcut("Ctrl+E")
+        openButton.setStatusTip("Open wav file")
+        openButton.triggered.connect(self.open)
+        fileMenu.addAction(openButton)
+        # Export file
+        exportButton = QAction('Export', self)
+        fileMenu.addAction(exportButton)
+        # Exit Button
+        exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitButton.setShortcut('Ctrl+Q')
+        exitButton.setStatusTip('Exit application')
+        exitButton.triggered.connect(self.close)
+        fileMenu.addAction(exitButton)
+
+    def open(self):
+        print('open file')
+        path = 'hola'
+        self.data.open_wav(path=path)
 
 if __name__ == '__main__':
-
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow('some/path', 'some/other/path')
-    window.show()
-    window.setGeometry(500, 300, 300, 300)
+    app = QApplication(sys.argv)
+    ex = App()
     sys.exit(app.exec_())
-
