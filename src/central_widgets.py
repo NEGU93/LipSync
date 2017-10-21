c47ba67419a18df49f79f86954e04d5d9771e6af
NEGU93 <joseagustin.barra@gmail.com>
2017-10-21 19:26:26 -0300

    Show image of mouth

--- src/central_widgets.py
+++ src/central_widgets.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QPushButton, QHBoxLayout, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QPushButton, QHBoxLayout, QWidget, QVBoxLayout, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
 
 
 class FormWidget(QWidget):
@@ -11,13 +12,21 @@ class FormWidget(QWidget):
 
         self.setLayout(self.layout)
 
-    def rightLayout_init(self):
-        self.rightLayout = QVBoxLayout(self)
-        self.button1 = QPushButton("Button 1")
-        self.rightLayout.addWidget(self.button1)
-        self.layout.addLayout(self.rightLayout)
-
     def leftLayout_init(self):
+        self.leftLayout = QVBoxLayout(self)
+
+        self.label = QLabel(self)
+        # import pdb; pdb.set_trace()
+        self.pixmap = QPixmap('../images/mouth_types.jpg')
+        self.label.setPixmap(self.pixmap)
+        self.leftLayout.addWidget(self.label)
+
+        self.button1 = QPushButton("Button 1")
+        self.leftLayout.addWidget(self.button1)
+
+        self.layout.addLayout(self.leftLayout)
+
+    def rightLayout_init(self):
         self.leftLayout = QVBoxLayout(self)
         self.button2 = QPushButton("Button 2")
         self.layout.addWidget(self.button2)
