9b5791cae33c407d4af447f2485a03b7463d70bb
MatiasDwek <matiasdwek@gmail.com>
2017-10-21 18:49:33 -0300

    Energy function working, mouth_types

--- src/process.py
+++ src/process.py
@@ -7,30 +7,39 @@ import wave
 import sounddevice as sd
 import sys
 import struct
-from processing_functions import shorttime_energy
+from processing_functions import short_time_energy
 
 
 def wav_to_floats(wave_file):
     w = wave.open(wave_file)
+    fs = w.getframerate()
     astr = w.readframes(w.getnframes())
     # convert binary chunks to short
     a = struct.unpack("%ih" % (w.getnframes()* w.getnchannels()), astr)
     a = [float(val) / pow(2, 15) for val in a]
-    return a
+    return a, fs
 
 
 filename = '../sounds/vocales.wav'
-wave_read = wave.open(filename, mode=None)
-fs = wave_read.getframerate()
-n = wave_read.getnframes()
-frames = wav_to_floats(filename)
+sound_frames, fs = wav_to_floats(filename)
+sound_frames = np.asarray(sound_frames)
+window = signal.hann(100)
+step_size = 50
+st_energy = short_time_energy(sound_frames, window, step_size)
+print(st_energy.size)
+print(sound_frames.size)
 
-print(frames[1])
-sd.play(frames, fs)
-plt.plot(frames)
+# sd.play(sound_frames, fs)
+plt.plot(sound_frames)
+plt.plot(st_energy)
+# print(st_energy)
 plt.show()
 
-x = np.linspace(-np.pi, np.pi, 200)
-sig = np.sin(x)
-window = signal.hann(100)
-print(shorttime_energy(sig, window, 50))
+# print(np.zeros((2, 5)))
+#
+# x = np.linspace(-np.pi, np.pi, 200)
+# sig = np.sin(x)
+# window = signal.hann(100)
+# ww = np.linspace(-np.pi, np.pi, 10)
+# print(ww[ww.size-1])
+# print(shorttime_energy(sig, window, 50))
--- src/processing_functions.py
+++ src/processing_functions.py
@@ -19,3 +19,35 @@ def shorttime_energy(sig, window, step_size):
         energy += frame_energy
 
     return energy
+
+
+def short_time_energy(signal, window, step_size):
+    energy_time = np.zeros(signal.size)
+
+    for it in range(0, math.ceil(signal.size / step_size)):
+        frame_signal = signal[it*step_size:min(it*step_size + window.size, signal.size)]
+        # import pdb; pdb.set_trace()
+        if window.size != frame_signal.size:
+            window = window[0:frame_signal.size]
+        if (frame_signal.size == 0) or (frame_signal.size == 0):
+            return energy_time
+
+        frame_energy = np.sum(np.square(np.multiply(frame_signal, window)))
+        energy_time[it * step_size: min(it * step_size + window.size, signal.size)] = frame_energy
+    return energy_time
+
+
+# energy_time = zeros(1, length(signal));
+#
+# for it = 0:ceil(length(signal) / step_size)
+# frame_signal = signal(it * step_size + 1:min(it * step_size + length(window), length(signal)));
+# if length(window) ~= length(frame_signal)
+# window = window(1:length(frame_signal));
+# end
+# if isempty(frame_signal) | | isempty(window)
+#     return
+# end
+# frame_energy = sum((frame_signal. * window). ^ 2);
+# energy_time(it * step_size + 1: min(it * step_size + length(window), length(signal))) = frame_energy;
+#
+# end
