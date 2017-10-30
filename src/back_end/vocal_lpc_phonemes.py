b67222dc9d9e5cdc175b6f58bad52ce3c8a02fae
NEGU93 <joseagustin_barra@hotmail.com>
2017-10-30 11:08:36 -0300

    Organized project

--- src/back_end/vocal_lpc_phonemes.py
+++ src/back_end/vocal_lpc_phonemes.py
@@ -1,15 +1,10 @@
-import numpy as np
 from scipy import signal
-from scipy.fftpack import fft, fftshift
-import matplotlib.pyplot as plt
 import math
 import wave
 import data
-import sys
 import struct
 from processing_functions import short_time_energy
 from processing_functions import formants_calculator
-from audiolazy import lazy_lpc
 from phonemes import *
 
 
@@ -27,7 +22,7 @@ def most_common(lst):
     return max(set(lst), key=lst.count)
 
 
-def process_audio():
+def vocal_phonemes():
     # filename = '../sounds/vocales.wav'
     datos = data.LipSyncData.get_instance()
     # sound_frames, fs = wav_to_floats(filename)
