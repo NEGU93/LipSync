cfdd2ea88026d1413c30e793d78564e5c16470b2
NEGU93 <joseagustin.barra@gmail.com>
2017-11-29 22:49:17 -0300

    trained epochs

--- liblip/train_RNN.py
+++ liblip/train_RNN.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
 
     # Train
     print('Training Net')
-    model.fit(features_train, label_train, batch_size=100, epochs=1, callbacks=[checkpoint, reduce_lr],
+    model.fit(features_train, label_train, batch_size=100, epochs=20, callbacks=[checkpoint, reduce_lr],
               validation_data=(features_test, label_test))
 
     model.save('../data/saved_rnn/model_adam.hdf5')
